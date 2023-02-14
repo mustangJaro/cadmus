af350cfe71b0614459151e97d591eccf54df3049
AJ <ajarosinski@gmail.com>
2023-02-14 13:11:55 -0600

    Update some styling

--- src/Components/InputForm/Input.js
+++ src/Components/InputForm/Input.js
@@ -1,11 +1,18 @@
-import styles from './Input.module.css';
+import styles from "./Input.module.css";
+
+import TextField from "@mui/material/TextField";
 
 const Input = (props) => {
   return (
     <div>
-      <label className={styles.label}>{props.label}</label>
-      <input className={styles.input} ref={props.valueRef} defaultValue={props.defaultValue || ''}/>
+      <TextField
+        required
+        className={styles.label}
+        ref={props.valueRef}
+        defaultValue={props.defaultValue || ""}
+        label={props.label}
+      />
     </div>
-  )
-}
+  );
+};
 export default Input;
--- src/Components/InputForm/InputForm.js
+++ src/Components/InputForm/InputForm.js
@@ -1,6 +1,7 @@
-import { useRef } from 'react';
-import Input from './Input';
-import styles from './InputForm.module.css';
+import { useRef } from "react";
+import Input from "./Input";
+import styles from "./InputForm.module.css";
+import Button from "@mui/material/Button";
 
 const InputForm = (props) => {
   const investmentValueRef = useRef();
@@ -13,25 +14,49 @@ const InputForm = (props) => {
   const onSubmitHandler = (event) => {
     event.preventDefault();
     props.onSubmit({
-      "investmentAmount": investmentValueRef.current.value,
-      "equity": equityValueRef.current.value,
-      "interestRate": interestRateValueRef.current.value,
-      "loanYears": loanYearsValueRef.current.value,
-      "rentalIncome": rentalIncomeValueRef.current.value,
-      "expenses": expensesValueRef.current.value
+      investmentAmount: investmentValueRef.current.value,
+      equity: equityValueRef.current.value,
+      interestRate: interestRateValueRef.current.value,
+      loanYears: loanYearsValueRef.current.value,
+      rentalIncome: rentalIncomeValueRef.current.value,
+      expenses: expensesValueRef.current.value,
     });
-  }
+  };
 
   return (
-    <form className={styles['input-form']}>
-      <Input valueRef={investmentValueRef} label="Investment Amount" defaultValue='215000'/>
-      <Input valueRef={equityValueRef} label="Equity" defaultValue='20'/>
-      <Input valueRef={interestRateValueRef} label="Interest Rate" defaultValue='4.5'/>
-      <Input valueRef={loanYearsValueRef} label="Loan Duration, in years" defaultValue='30'/>
-      <Input valueRef={rentalIncomeValueRef} label="Rental Income" defaultValue='25000'/>
-      <Input valueRef={expensesValueRef} label="Expenses" defaultValue='10500'/>
-      <button onClick={onSubmitHandler}>Calculate</button>
-    </form>
-  )
-}
+    <div className={styles.container}>
+      <form className={styles["input-form"]}>
+        <Input
+          valueRef={investmentValueRef}
+          label="Investment Amount"
+          defaultValue="215000"
+        />
+        <Input valueRef={equityValueRef} label="Equity" defaultValue="20" />
+        <Input
+          valueRef={interestRateValueRef}
+          label="Interest Rate"
+          defaultValue="4.5"
+        />
+        <Input
+          valueRef={loanYearsValueRef}
+          label="Loan Duration, in years"
+          defaultValue="30"
+        />
+        <Input
+          valueRef={rentalIncomeValueRef}
+          label="Rental Income"
+          defaultValue="25000"
+        />
+        <Input
+          valueRef={expensesValueRef}
+          label="Expenses"
+          defaultValue="10500"
+        />
+        <Button variant="contained" onClick={onSubmitHandler}>
+          Calculate
+        </Button>
+      </form>
+    </div>
+  );
+};
 export default InputForm;
