887ea1a07781c8a13051340fbf20036f5263fc85
AJ <ajarosinski@gmail.com>
2023-02-18 09:56:34 -0600

    Update output colors

--- src/Components/CalculatedOutput/CalculatedOutput.js
+++ src/Components/CalculatedOutput/CalculatedOutput.js
@@ -2,18 +2,19 @@ import styles from "./CalculatedOutput.module.css";
 import LineItem from "../UI/LineItem";
 import { Percent, Dollars } from "../../Helpers/Format";
 
-
 const CalculatedOutput = (props) => {
   const debtService = props.monthlyPayment * 12 * -1;
   const netCashFlow = props.netOperatingIncome + debtService;
   const leveragedROI = (netCashFlow / props.equity) * 100;
-  const rOIAfterPrincipal = (netCashFlow + props.firstYearPrincipal) / props.equity * 100;
+  const rOIAfterPrincipal =
+    ((netCashFlow + props.firstYearPrincipal) / props.equity) * 100;
   return (
     <div className={styles["container"]}>
       <div className={styles["calculated-output"]}>
         <LineItem
           label="Capitalization Rate"
           value={Percent(props.capitalizationRate)}
+          type="calculated"
         />
         <LineItem
           label="Investment Amount"
@@ -24,10 +25,19 @@ const CalculatedOutput = (props) => {
         <LineItem
           label="Net Operating Income"
           value={Dollars(props.netOperatingIncome)}
+          type="summary"
         />
         <LineItem label="Debt Service" value={Dollars(debtService)} />
-        <LineItem label="Net Cash Flow" value={Dollars(netCashFlow)} />
-        <LineItem label="Leveraged ROI" value={Percent(leveragedROI)} />
+        <LineItem
+          label="Net Cash Flow"
+          value={Dollars(netCashFlow)}
+          type="summary"
+        />
+        <LineItem
+          label="Leveraged ROI"
+          value={Percent(leveragedROI)}
+          type="calculated"
+        />
         <LineItem
           label="Principal Payment Year 1"
           value={Dollars(props.firstYearPrincipal)}
@@ -35,8 +45,12 @@ const CalculatedOutput = (props) => {
         <LineItem
           label="ROI After Principal Payment"
           value={Percent(rOIAfterPrincipal)}
+          type="calculated"
         />
-        <LineItem label="Interest Year 1" value={Dollars(props.firstYearInterest)} />
+        <LineItem
+          label="Interest Year 1"
+          value={Dollars(props.firstYearInterest)}
+        />
         <LineItem label="Tax Refund on Interest" value="calc" />
         <LineItem label="Tax Refund on Expenses" value="calc" />
       </div>
--- src/Components/UI/LineItem.js
+++ src/Components/UI/LineItem.js
@@ -1,9 +1,19 @@
 import styles from "./LineItem.module.css";
-import Text from './Text';
+import Text from "./Text";
 
 const LineItem = (props) => {
+  let cssType = "detail";
+  switch (props.type) {
+    case "summary":
+      cssType = "summary";
+      break;
+    case "calculated":
+      cssType = "calculated";
+      break;
+  }
+
   return (
-    <div className={styles['line-item']}>
+    <div className={`${styles["line-item"]} ${styles[cssType]}`}>
       <Text value={props.label} />
       <Text value={props.value} />
     </div>
